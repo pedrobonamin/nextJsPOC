ad3a1b63cb4469e03ce01c038a8621087f3f3aad
pedrobonamin <pedrobonamin@gmail.com>
2020-07-31 15:22:35 -0300

    Midudev first chapter

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import '../styles/globals.css'
 
+
+// Este componente es el inicial de la aplicación es un lugar adecuado para persistir
+// todos los estilos o librerias que necesitemos en toda la app. 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
---
+++ pages/timeline/index.js
@@ -0,0 +1,37 @@
+import Link from 'next/Link'
+
+export default function Timeline({ userName }) {
+  return (
+    <>
+      <h1>This is the timeline of {userName}</h1>
+      <Link href='/'>
+        <a>
+          go Home
+        </a>
+      </Link>
+      <style jsx>{`
+      h1 {
+        font-size: 36px;
+        color: #333
+      }
+      `}
+      </style>
+    </>
+  )
+}
+
+// Este metodo se ejecuta en el servidor.
+// Si tiene el método get initial props, se ejecuta antes de renderizrlo.
+// Esto funciona solo en componentes tipo Página, no en components o subcomponentes.
+
+// IMPORTANTE:
+// Si entras directo a la ruta, se ejecuta en el servidor.
+// Si entras desde otra página como PWA, se ejecuta en el cliente.
+Timeline.getInitialProps = () => {
+  return fetch('http://localhost:3000/api/hello')
+    .then(res => res.json())
+    .then(response => {
+      const { userName } = response
+      return response
+    })
+}
